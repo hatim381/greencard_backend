c8a6af8427a87d8f228de6b5cef472a9562afe5b
yuba0 <rahaliyuba@gmail.com>
2025-08-29 11:12:09 +0200

    push apres autorisation des clefs

--- fonctions/add_order_columns.py
+++ fonctions/add_order_columns.py
@@ -18,7 +18,7 @@ def add_order_columns():
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
         
-        # Récupérer la structure actuelle de la table orders
+        # Récupérer la structure actuelle de la table ordersd
         cursor.execute("PRAGMA table_info(orders);")
         columns = [row[1] for row in cursor.fetchall()]
         print(f"Colonnes actuelles dans orders: {columns}")
